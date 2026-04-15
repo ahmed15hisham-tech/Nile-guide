b54e3738e413d58c31713a15316ef927fad8a9bd
youssefpower <youssefpower777@gmail.com>
2026-04-15 20:16:13 +0200

    Admin Report

--- src/app/features/admin/dashboard/dashboard.component.ts
+++ src/app/features/admin/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {  OnDestroy, OnInit } from '@angular/core';
+import {NavigationStart, NavigationEnd, Router,} from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +10,44 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, OnDestroy {
 
+   private routerSub?: Subscription;
+  private savedScrollY = 0;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.routerSub = this.router.events
+      .pipe(
+        filter(
+          (event) =>
+            event instanceof NavigationStart || event instanceof NavigationEnd
+        )
+      )
+      .subscribe((event) => {
+        const currentUrl = this.router.url;
+
+        const isInsideDashboard =
+          currentUrl.startsWith('/dashboard') ||
+          (event instanceof NavigationStart &&
+            event.url.startsWith('/dashboard'));
+
+        if (!isInsideDashboard) return;
+
+        if (event instanceof NavigationStart) {
+          this.savedScrollY = window.scrollY;
+        }
+
+        if (event instanceof NavigationEnd) {
+          setTimeout(() => {
+            window.scrollTo(0, this.savedScrollY);
+          });
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.routerSub?.unsubscribe();
+  }
 }
